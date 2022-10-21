eb91ec6ce9e33aa4383353ca79c0c12e0153145a
Pascal Straßel <pascal.strassel@iem.thm.de>
2022-10-21 21:40:31 +0200

    Arbeit an Volumeslider

--- app/src/main/java/de/thm/lampgame/Settings.kt
+++ app/src/main/java/de/thm/lampgame/Settings.kt
@@ -1,12 +1,27 @@
 package de.thm.lampgame
 
-import android.app.Activity
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.net.Uri
 
-class Settings : Activity() {
+
+class Settings : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
 
+        val seek = findViewById<SeekBar>(R.id.volbar)
+        val audioManager: AudioManager
+
+
     }
+
 }
