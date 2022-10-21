e034e7d945aee416bc4be0df3c7076cb032e72d6
Anastasios Kistoglidis <anastasios.kistoglidis@iem.thm.de>
2022-10-21 16:17:37 +0200

    Superclass Obstacles for Obstacle-Variety in Tilesets

---
+++ app/src/main/java/de/thm/lampgame/Obstacles.kt
@@ -0,0 +1,14 @@
+package de.thm.lampgame
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+
+open abstract class Obstacles(context: Context,val height: Int,val width: Int,var x: Int,var y: Int) {
+
+    lateinit var unsizedBmp: Bitmap
+    lateinit var bmp: Bitmap
+
+    abstract fun draw(canvas: Canvas, velocity: Int)
+}
--- app/src/main/java/de/thm/lampgame/TilesetQueue.kt
+++ app/src/main/java/de/thm/lampgame/TilesetQueue.kt
@@ -5,14 +5,23 @@ class TilesetQueue {
 
     fun initQueue(t1: Tileset, t2: Tileset, screenWidth: Int){
         queue.add(t1);queue.add(t2)
-        queue.first().obstacles.forEach {it.terrainX += screenWidth}
-        queue.last().obstacles.forEach {it.terrainX += screenWidth * 2}
+
+        for (i in 0 until queue.first().obstacles.size) {
+            queue.first().obstacles[i].x += screenWidth
+        }
+
+        for (i in 0 until queue.last().obstacles.size) {
+            queue.last().obstacles[i].x += screenWidth * 2
+        }
+        //Alternative Lösung für die for-Schleife mit ClassCastException
+        //queue.first().obstacles.forEach {(it as Obstacles).x  += screenWidth}
+        //queue.last().obstacles.forEach {(it as Obstacles).x += screenWidth * 2}
     }
 
     fun insertTileset(screenWidth: Int, t: Tileset){
             queue.removeFirst()
             queue.add(t)
-            queue.last().obstacles.forEach {it.terrainX += screenWidth}
+            queue.last().obstacles.forEach {it.x += screenWidth}
 
     }
 }
